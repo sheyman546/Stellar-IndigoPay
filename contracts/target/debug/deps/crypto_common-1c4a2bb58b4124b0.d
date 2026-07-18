1087307b734b9d91fb8a3bbe0171a4c13152cd0e
Lewechi <lewechigodsfavour@gmail.com>
2026-07-18 18:25:45 +0100

    fix: fixes

--- contracts/target/debug/deps/crypto_common-1c4a2bb58b4124b0.d
+++ contracts/target/debug/deps/crypto_common-1c4a2bb58b4124b0.d
@@ -1,7 +1,7 @@
-C:\projetcs\codezeus\zendvo\smart_contracts\target\debug\deps\crypto_common-1c4a2bb58b4124b0.d: C:\Users\TCE\ HUB\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crypto-common-0.1.6\src\lib.rs
+C:\projetcs\codezeus\zendvo\contracts\target\debug\deps\crypto_common-1c4a2bb58b4124b0.d: C:\Users\TCE\ HUB\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crypto-common-0.1.6\src\lib.rs
 
-C:\projetcs\codezeus\zendvo\smart_contracts\target\debug\deps\libcrypto_common-1c4a2bb58b4124b0.rlib: C:\Users\TCE\ HUB\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crypto-common-0.1.6\src\lib.rs
+C:\projetcs\codezeus\zendvo\contracts\target\debug\deps\libcrypto_common-1c4a2bb58b4124b0.rlib: C:\Users\TCE\ HUB\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crypto-common-0.1.6\src\lib.rs
 
-C:\projetcs\codezeus\zendvo\smart_contracts\target\debug\deps\libcrypto_common-1c4a2bb58b4124b0.rmeta: C:\Users\TCE\ HUB\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crypto-common-0.1.6\src\lib.rs
+C:\projetcs\codezeus\zendvo\contracts\target\debug\deps\libcrypto_common-1c4a2bb58b4124b0.rmeta: C:\Users\TCE\ HUB\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crypto-common-0.1.6\src\lib.rs
 
 C:\Users\TCE\ HUB\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crypto-common-0.1.6\src\lib.rs:
