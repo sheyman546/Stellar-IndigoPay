4e21396519e3e9e91f8c3c19e68c13fc62abf1e7
kosiso <project.kosiso@gmail.com>
2026-07-20 16:38:58 +0100

    fix(frontend): add framer-motion declaration overrides to pass compilation

---
+++ frontend/types/framer-motion.d.ts
@@ -0,0 +1,7 @@
+declare module "framer-motion" {
+  import * as React from "react";
+  export const motion: any;
+  export const AnimatePresence: any;
+  export function useReducedMotion(): boolean;
+  export type Variants = any;
+}
